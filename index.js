44a16532f3d7b7e9474f220697ce1d9723728dae
John Hill <john.m.hill91@gmail.com>
2019-01-14 19:32:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function deleteFromObjectByKey(object, key){
   return clone
 }
 
-function destructivelyDeleteFromObjectsByKey(object, key){
+function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key]
   return object
 }
