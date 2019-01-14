187d351f2f96832ef4eac67e3cf23d8535ea9d9c
John Hill <john.m.hill91@gmail.com>
2019-01-14 19:21:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value){
    return Object.assign({},object,{[key]:value})
 }
 
-function destuctivelyUpdateObjectWithKeyAndValue(object, key, value){
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
  delete object.key
   return object
 }
