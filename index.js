75851440cdc84ea65193b9c52ede2fdc7206d9ff
John Hill <john.m.hill91@gmail.com>
2019-01-14 19:28:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var clone=object.assign{{},object}
+  var clone= object.assign{{},object}
   
 }
